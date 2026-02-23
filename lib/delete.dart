19cb6ff4a568123c57d1eda6d4ebb4c253b3677b
CHHAIRINN <chhaiarinns@gmail.com>
2026-02-23 09:58:16 +0700

    add and update delete.dart

---
+++ lib/delete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Delete extends StatefulWidget {
+  const Delete({super.key});
+
+  @override
+  State<Delete> createState() => _DeleteState();
+}
+
+class _DeleteState extends State<Delete> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("test delete");
+  }
+}
